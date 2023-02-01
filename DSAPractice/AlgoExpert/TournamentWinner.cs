d1bae6f0e3c237643afcc468a7053f4e09310f1e
= <kinkeadian@outlook.com>
2023-02-01 10:23:11 -0800

    boom

--- DSAPractice/AlgoExpert/TournamentWinner.cs
+++ DSAPractice/AlgoExpert/TournamentWinner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Net.Sockets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,22 +11,21 @@ namespace DSAPractice.AlgoExpert
     {
         public string TournamentWinnerMethod(List<List<string>> competitions, List<int> results)
         {
-            Dictionary<string, int> score = new Dictionary<string, int>();
+            var score = new Dictionary<string, int>();
 
-            for (int i = 0; i < competitions.Count; i++)
+            for (var i = 0; i < competitions.Count; i++)
             {
                 var testWinner = results[i] == 1 ? competitions[i][0] : competitions[i][1];
                 if (score.ContainsKey(testWinner))
                 {
                     score[testWinner] += 3;
                 }
-                else
+                else 
                 {
                     score.Add(testWinner, 3);
                 }
-
             }
-            return score.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+            return score.Aggregate((l,r) => l.Value > r.Value ? l : r).Key;
         }
     }
 }
